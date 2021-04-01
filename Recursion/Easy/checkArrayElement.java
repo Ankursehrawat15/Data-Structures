2a408435b21297ad836f99f19b5a011ec3c0cc42
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-01 15:43:56 +0530

    Create checkArrayElement.java

---
+++ Recursion/Easy/checkArrayElement.java
@@ -0,0 +1,32 @@
+//  Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.  
+
+
+
+
+public static boolean checkNumber(int input[], int x, int index) {
+		
+        if(index == input.length) return false;
+        
+        
+        boolean  smallAns = checkNumber(input , x , index+1);
+      
+        if(!(smallAns)){
+             if(input[index] == x ){
+          return true;          
+        }else{
+            return false;
+        }
+        }
+        else{
+            return true;
+        }
+      
+      
+	}
+	
+	public static boolean checkNumber(int input[], int x) {
+		
+        return checkNumber(input , x , 0);
+        
+        
+	}
