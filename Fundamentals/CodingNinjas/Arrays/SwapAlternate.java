090a6046dd028cf0a499428ece78e29534cbc7cf
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-03 01:49:42 +0530

    Delete Fundamentals/CodingNinjas/Arrays directory

--- Fundamentals/CodingNinjas/Arrays/SwapAlternate.java
+++
@@ -1,9 +0,0 @@
-public static void swapAlternate(int arr[]) {
-    	
-        for(int i =0;i<arr.length-1;i+=2){
-            
-            int temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-            
-        }
