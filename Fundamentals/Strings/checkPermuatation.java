b5cd8a73c8849ae53e4e1c5507d14bd350a51afc
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-03 14:30:05 +0530

    Create checkPermuatation.java

---
+++ Fundamentals/Strings/checkPermuatation.java
@@ -0,0 +1,29 @@
+	public static boolean isPermutation(String str1, String str2) {
+      
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        
+        
+		  int count =0;
+        for(int i =0; i<str1.length();i++){
+            
+          
+            for(int j=0;j<str2.length();j++){
+                
+                if(str1.charAt(i) == str2.charAt(j)){
+                    count++;
+                }
+                
+            }
+            
+    
+        }
+        
+        if(count == str1.length()){
+            return true;
+        }else{
+            return false;
+        }
+        
+	}
