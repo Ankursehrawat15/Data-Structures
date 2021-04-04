a9e7657e0a0055cc03f921edc4a8331f923ed80b
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-04 16:48:39 +0530

    Update HighestOccurence.java

--- Fundamentals/Strings/HighestOccurence.java
+++ Fundamentals/Strings/HighestOccurence.java
@@ -30,3 +30,28 @@ public static char highestOccuringChar(String str) {
 
 
 // better approach
+	public static char highestOccuringChar(String str) {
+		
+    int [] frequency = new int [256];
+        int maxFrequency = 0;
+        
+        for(int i =0 ;i<str.length();i++){
+            
+            frequency[str.charAt(i)]++;
+            
+            maxFrequency = Math.max(maxFrequency , frequency[str.charAt(i)]);
+        }
+        
+        char ans = '0';
+        for(int i =0;i<frequency.length;i++){
+            
+            if(frequency[str.charAt(i)] == maxFrequency){
+                ans = str.charAt(i);
+                break;
+            }
+            
+        }
+            
+        return ans;
+        
+	}
