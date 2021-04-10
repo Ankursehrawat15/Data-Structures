bb722e6d793a0bab4e30e09b0e76f7cb23d72a23
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-10 12:49:34 +0530

    Create MergeLinkedList.java

---
+++ Fundamentals/LinkedList/MergeLinkedList.java
@@ -0,0 +1,33 @@
+// Merge Two Sorted Linked List
+
+// Time Complexity ==> o(n + m)
+static Node<Integer> mergeLinkedList(Node<Integer> head1 , Node<Integer> head2){
+		
+		Node<Integer> l1= head1;
+		Node<Integer> l2= head2;
+		
+		Node<Integer> newList = new Node<>(-1);
+		Node<Integer> tail = newList;
+		while(l1 != null && l2 != null) {
+			
+			if(l1.data < l2.data) {
+				
+				tail.next = l1;
+				l1 = l1.next;
+				
+			}else {
+				
+				tail.next = l2;
+				l2 = l2.next;
+			}
+			
+			tail = tail.next;
+		}
+		
+		tail.next = l1 != null ? l1 : l2;
+		
+		
+		return newList.next;
+		
+	}
+	
