ef0d3b6c316f9f6fee48c85310be691e0ecf0c5a
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-19 22:08:53 +0530

    Update ReverseStack.java

--- Fundamentals/Stacks&Queues/ReverseStack.java
+++ Fundamentals/Stacks&Queues/ReverseStack.java
@@ -1,3 +1,4 @@
+// In this approach i am using an Array
 int arr[] = new int[input.size()];
 	
 	for(int i =0; i <arr.length;i++) {
@@ -16,3 +17,25 @@ int arr[] = new int[input.size()];
 		int temp = extra.pop();
 		input.push(temp);
 	}
+
+
+// using Recursion
+int arr[] = new int[input.size()];
+
+	for (int i = 0; i < arr.length; i++) {
+
+		arr[i] = input.pop();
+
+	}
+
+	for (int i = arr.length - 1; i >= 0; i--) {
+
+		extra.push(arr[i]);
+	}
+
+	while (extra.size() != 0) {
+
+		int temp = extra.pop();
+		input.push(temp);
+	}
+	
