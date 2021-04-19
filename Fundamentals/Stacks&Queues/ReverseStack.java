6d81c32b470d9f16683c1f6292d86cc98241a2d6
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-19 21:35:13 +0530

    Create ReverseStack.java

---
+++ Fundamentals/Stacks&Queues/ReverseStack.java
@@ -0,0 +1,18 @@
+int arr[] = new int[input.size()];
+	
+	for(int i =0; i <arr.length;i++) {
+		
+		arr[i] = input.pop();
+		
+	}
+	
+	for(int i=arr.length-1;i>= 0;i--) {
+		
+		extra.push(arr[i]);
+	}
+	
+	while(extra.size() != 0) {
+		
+		int temp = extra.pop();
+		input.push(temp);
+	}
