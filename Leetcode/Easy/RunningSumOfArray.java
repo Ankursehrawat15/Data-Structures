1c4141184ac160eb516a7445119feed3a419ee9f
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-21 15:24:29 +0530

    Create RunningSumOfArray.java

---
+++ Leetcode/Easy/RunningSumOfArray.java
@@ -0,0 +1,27 @@
+// o(n) 
+// Second Approach
+int [] arr = new int[nums.length];
+        int prevSum = 0;
+        int sum =0;
+        for(int i =0;i<arr.length;i++){
+        
+            sum = prevSum + nums[i];
+            prevSum = sum;
+            arr[i] = prevSum;
+        }
+        
+        return arr;
+    }
+
+// first Approach
+ int [] arr = new int[nums.length];
+        for(int i =0;i<arr.length;i++){
+            int sum =0;
+            for(int j =0;j<= i;j++){
+                sum += nums[j];
+
+            }
+                            arr[i] = sum;
+        }
+        
+        return arr;
