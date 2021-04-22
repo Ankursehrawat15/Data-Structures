6b48907b9763976a363504446ec49aa7b52fcb56
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-04-22 19:55:35 +0530

    Update MaxSumNode.java

--- Fundamentals/Trees/MaxSumNode.java
+++ Fundamentals/Trees/MaxSumNode.java
@@ -29,3 +29,46 @@
 		return maxNode;
 
 	}
+
+// Another approach
+	public class Solution {
+
+		static class Pair<T> {
+			TreeNode<T> node;
+			int sum;
+		}
+
+		public static Pair<Integer> maxSum(TreeNode<Integer> root) {
+			Pair<Integer> maxNode = new Pair<>();
+			if (root == null)
+				return maxNode;
+			int maxsum = root.data;
+
+			for (int i = 0; i < root.children.size(); i++) {
+				maxsum += root.children.get(i).data;
+
+			}
+
+			maxNode.node = root;
+			maxNode.sum = maxsum;
+
+			for (int i = 0; i < root.children.size(); i++) {
+				Pair<Integer> subMaxNode = maxSum(root.children.get(i));
+				if (subMaxNode.sum > maxNode.sum) {
+					maxNode.node = subMaxNode.node;
+					maxNode.sum = subMaxNode.sum;
+				}
+
+			}
+
+			return maxNode;
+
+		}
+
+		public static TreeNode<Integer> maxSumNode(TreeNode<Integer> root) {
+
+			return maxSum(root).node;
+
+		}
+
+	}
