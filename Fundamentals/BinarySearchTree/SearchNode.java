a438c3f69ac64f9898dc17b885fafc188e1f9336
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-04 18:00:40 +0530

    Update SearchNode.java

--- Fundamentals/BinarySearchTree/SearchNode.java
+++ Fundamentals/BinarySearchTree/SearchNode.java
@@ -25,3 +25,23 @@
 	}
 
 }
+
+
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+        
+        if(root == null) return false;
+        
+        if(root.data == k){
+            return true;
+        }else if (k < root.data){
+            
+          return searchInBST(root.left , k);
+            
+        } else {
+            return searchInBST(root.right , k);
+        }
+        
+      
+		
+	}
+}
