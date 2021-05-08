f8cac7de27aa0c1c3c33ac2b71c9fcea92102a50
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-08 19:47:47 +0530

    Create shivamLearn.cpp

---
+++ CodeChef/shivamLearn.cpp
@@ -0,0 +1,19 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main(){
+int t ;
+cin>>t;
+
+while(t--){
+	int x,y;
+	cin>>x>>y;
+
+	cout<<(x+y)<<endl;
+}
+return 0;
+
+}
