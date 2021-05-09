a2b6ed435ad11a4ea109176fe138a41748cb81b5
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-09 12:45:30 +0530

    Update WeiredAlgorithm.cpp

--- CodeChef/WeiredAlgorithm.cpp
+++ CodeChef/WeiredAlgorithm.cpp
@@ -26,3 +26,27 @@ Output:
 
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+	long n;
+	cin>>n;
+	cout<<n<<" ";
+	while(n != 1){
+
+		if(n % 2 == 0){
+			n /= 2;
+
+		}else{
+			n = n * 3 + 1;
+
+		}
+		cout<<n<<" ";
+      }
+}
+
+
