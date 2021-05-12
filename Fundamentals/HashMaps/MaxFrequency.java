41cd3425099c2d0caedd54d77b7578310d362494
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-12 19:12:32 +0530

    Update MaxFrequency.java

--- Fundamentals/HashMaps/MaxFrequency.java
+++ Fundamentals/HashMaps/MaxFrequency.java
@@ -1,1 +1,34 @@
-?
+// not passing all test cases
+    public static int maxFrequencyNumber(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+
+                int v = map.get(arr[i]);
+                v++;
+                map.put(arr[i], v);
+            }
+
+            map.put(arr[i], 1);
+
+        }
+
+        int max = Integer.MIN_VALUE;
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            int val = map.get(arr[i]);
+            if (val > max) {
+                max = val;
+                num = arr[i];
+
+            }
+
+        }
+
+
+        return num;
+
+
+    }
