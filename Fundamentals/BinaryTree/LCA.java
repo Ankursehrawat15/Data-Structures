41d33637916f6bc89e6f7e6dd433b673c8586643
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-12 20:53:25 +0530

    Update LCA.java

--- Fundamentals/BinaryTree/LCA.java
+++ Fundamentals/BinaryTree/LCA.java
@@ -24,4 +24,43 @@ static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
 
 
 // better code approach 
+  public static BinaryTreeNode<Integer> nodeLCA(BinaryTreeNode<Integer> root, int a, int b) {
+
+        if (root == null) {
+            return null;
+        }
+
+
+        if (root.data == a || root.data == b) {
+
+            return root;
+        }
+
+        BinaryTreeNode<Integer> leftNode = nodeLCA(root.left, a, b);
+        BinaryTreeNode<Integer> rightNode = nodeLCA(root.right, a, b);
+
+        if (leftNode != null && rightNode != null) {
+            return root;
+        } else if (leftNode != null) {
+            return leftNode;
+        } else {
+            return rightNode;
+        }
+
+
+    }
+
+
+    static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+
+        BinaryTreeNode<Integer> ans = nodeLCA(root, a, b);
+        if (ans != null) {
+            return ans.data;
+        } else {
+            return -1;
+        }
+
+
+    }
+
 
