91b2b4778b9b73008b509cd278cd92332f487b6c
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-12 14:41:26 +0530

    Create RemoveDuplicate.java

---
+++ Fundamentals/HashMaps/RemoveDuplicate.java
@@ -0,0 +1,15 @@
+// using HashMpas
+public static ArrayList<Integer> removeDuplicates(int[] arr) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        ArrayList<Integer> newList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                continue;
+            }
+            newList.add(arr[i]);
+            map.put(arr[i], true);
+        }
+
+
+        return newList
+    }
