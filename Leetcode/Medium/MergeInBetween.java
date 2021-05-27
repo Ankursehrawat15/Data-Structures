dc16379d52d0c1a8d44d7946524d6c8a7d3d589f
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-27 23:38:57 +0530

    Update MergeInBetween.java

--- Leetcode/Medium/MergeInBetween.java
+++ Leetcode/Medium/MergeInBetween.java
@@ -1,35 +1,42 @@
 // Approach 1 --> Time complexity O(N)
 public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
     
-    ListNode rest = list1;
-        int i = 0;
-        while(i < b){
-            rest = rest.next;
-            i++;
+    ListNode currentNode = list1;
+ListNode bthNode = null;
+ListNode athNode = null;
+    int i =0;
+    while(currentNode != null){
+        
+        if(i == a-1){
+            athNode = currentNode;
         }
         
-       
-        rest = rest.next;
+        if(i == b){
+            bthNode = currentNode.next;
+        }
         
-        ListNode temp = list2;
-        while(temp.next != null){
-            temp = temp.next;
-        }
-          if(rest != null){
-        temp.next = rest;
-        }
-    
-       temp = list1;
-        i = 0;
-        while(i < a-1){
-            temp = temp.next;
-            i++;
-        }
-        temp.next = list2;
-    
-      
-        return list1;
         
+        i++;
+        currentNode = currentNode.next;
         
         
     }
+    
+    ListNode temp = list2;
+  
+    while(temp.next != null){
+        temp = temp.next;
+      
+    }
+    temp.next = bthNode;
+   athNode.next = list2;
+  
+  
+    return list1;
+    
+    
+    
+}
+
+    
+   
