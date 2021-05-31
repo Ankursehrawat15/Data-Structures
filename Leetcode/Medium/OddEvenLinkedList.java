bb3d7d169e9383c67553cd413fd7cd8615c873d1
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-05-31 11:05:11 +0530

    Update OddEvenLinkedList.java

--- Leetcode/Medium/OddEvenLinkedList.java
+++ Leetcode/Medium/OddEvenLinkedList.java
@@ -60,4 +60,26 @@ Link : https://leetcode.com/problems/odd-even-linked-list/submissions/
 
 	}
 
-// Approach2 
+// Approach2 in One space
+ public ListNode oddEvenList(ListNode head) {
+        
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        
+	 while(even != null && even.next != null){
+            
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+		 
+         }
+	 
+        odd.next = evenHead;
+        return head;
+        
+     }
