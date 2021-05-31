a203838445a692f4a912efdaafc6fe4ad28bcc15
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-01 02:10:05 +0530

    Update ReverseLinkedList.java

--- Leetcode/Easy/ReverseLinkedList.java
+++ Leetcode/Easy/ReverseLinkedList.java
@@ -42,3 +42,27 @@ public ListNode reverseList(ListNode head) {
         return reversed;
         
     }
+
+
+// Iterative Approach
+ public ListNode reverseList(ListNode head) {
+        
+        if(head == null || head.next == null) return head;
+         
+       ListNode curr = head;
+        ListNode prev = null;
+        
+        while(curr != null){
+            
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            
+            
+            
+        }
+        
+        return prev;
+        
+    }
