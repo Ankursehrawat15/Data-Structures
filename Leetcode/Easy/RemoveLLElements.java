74ee34101a4097686f41c7b6557d15f3e48e4005
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-01 11:50:34 +0530

    Update RemoveLLElements.java

--- Leetcode/Easy/RemoveLLElements.java
+++ Leetcode/Easy/RemoveLLElements.java
@@ -9,3 +9,14 @@ LINK: https://leetcode.com/problems/remove-linked-list-elements/
       
         
     }
+
+// Approach 2 iterative 
+ if(head == null) return head;
+    ListNode temp = head;
+    while(temp.next != null){
+        if(temp.next.val == val) temp.next = temp.next.next;
+        else temp = temp.next;
+    }
+    
+    return head.val == val ? head.next : head;
+}
