6dbf997804ecc47255ba063dc9df8aa842105447
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-04 23:01:48 +0530

    Update NextGreaterElement.java

--- Leetcode/Easy/NextGreaterElement.java
+++ Leetcode/Easy/NextGreaterElement.java
@@ -32,3 +32,29 @@ public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 		return arr;
 
 	}
+
+// Stacks and HashMaps
+	public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+		Stack<Integer> stack = new Stack<>();
+
+		for (int num : nums2) {
+
+			while (!stack.isEmpty() && num > stack.peek()) {
+
+				map.put(stack.pop(), num);
+			}
+
+			stack.push(num);
+		}
+
+		for (int i = 00; i < nums1.length; i++) {
+
+			nums1[i] = map.getOrDefault(nums1[i], -1);
+
+		}
+
+		return nums1;
+
+	}
