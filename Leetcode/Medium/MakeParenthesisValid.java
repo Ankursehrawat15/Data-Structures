22c485d5f6e027e6e32faa7cedeac9c91941a5fe
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-09 13:54:30 +0530

    lc : 921

---
+++ Leetcode/Medium/MakeParenthesisValid.java
@@ -0,0 +1,22 @@
+// using Stacks
+// Time Complexity O(N) space as well
+ public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                stack.push(s.charAt(i));
+            }else if(s.charAt(i) == ')'){
+                
+                if(stack.isEmpty()){
+                    count++;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        
+        count += stack.size();
+        
+        return count;
+    }
