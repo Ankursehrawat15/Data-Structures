a9f639abef2fce3c908b1bfd409a8a6164652a54
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-12 18:10:01 +0530

    Create ValidParenthesis.java

---
+++ Leetcode/Easy/ValidParenthesis.java
@@ -0,0 +1,38 @@
+// Time Complexity O(N) and space Complexity O(N) 
+public boolean isValid(String s) {
+
+		if (s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}')
+			return false;
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+				stack.push(s.charAt(i));
+
+			} else {
+				if (stack.size() > 0) {
+
+					if (s.charAt(i) == ')' && stack.peek() == '(') {
+
+						stack.pop();
+
+					} else if (s.charAt(i) == ']' && stack.peek() == '[') {
+
+						stack.pop();
+					} else if (s.charAt(i) == '}' && stack.peek() == '{') {
+						stack.pop();
+					} else {
+						return false;
+					}
+
+				} else {
+					return false;
+				}
+
+			}
+
+		}
+
+		return stack.isEmpty();
+
+	}
