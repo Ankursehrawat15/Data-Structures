7b718765eb9ec1f7e199221148389204fb81acf4
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-14 01:24:51 +0530

    lc:1019

--- Leetcode/Medium/NextGreaterNode.java
+++ Leetcode/Medium/NextGreaterNode.java
@@ -40,3 +40,50 @@ public int[] nextLargerNodes(ListNode head) {
         
         
     }
+
+
+// Approach 2nd using Stacks
+   // creates Array of linkedList
+   public int[] createArray(ListNode head){
+     ArrayList<Integer> list = new ArrayList<>();
+          ListNode node = head;
+        while(node != null){
+            list.add(node.val);
+            node = node.next;
+        }
+        
+        int [] ans = new int[list.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i] = list.get(i);
+        }
+       
+       return ans;
+    }
+     // finding next Greater Element
+public int[] nextLargerNodes(ListNode head) {
+        if(head == null){
+            return new int[0];
+        }
+    
+        int [] ans = createArray(head);
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<ans.length;i++){
+            
+            while(!stack.isEmpty() && ans[i] > ans[stack.peek()]){
+                int index = stack.pop();
+                ans[index] = ans[i];
+            }
+            
+            stack.push(i);
+        }
+        
+        
+     if(stack.size() != 0){
+         while(stack.size() > 0){
+             ans[stack.pop()] = 0;
+         }
+         
+     } 
+         return ans;
+             
+   }
