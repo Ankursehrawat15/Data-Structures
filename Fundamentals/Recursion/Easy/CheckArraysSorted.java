105750cebb3ece6faa189b3d4fabeb332167eb4a
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-15 21:51:42 +0530

    Create CheckArraysSorted.java

---
+++ Fundamentals/Recursion/Easy/CheckArraysSorted.java
@@ -0,0 +1,30 @@
+class Main {
+
+  // check array is Sorted
+  public static boolean checkSorted(int [] arr){
+      
+      if(arr.length <= 1){
+        return true;
+      }
+    int [] smallArr = new int[arr.length-1];
+    for(int i =1;i<arr.length;i++){
+      smallArr[i-1] = arr[i];
+    }
+
+    boolean ans = checkSorted(smallArr);
+    if(ans != true ){
+      return false;
+    }
+
+    if(arr[0] <= smallArr[0]){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  public static void main(String[] args) {
+    int [] arr = {1};
+    boolean  ans = checkSorted(arr);
+    System.out.println(ans);
+  }
+}
