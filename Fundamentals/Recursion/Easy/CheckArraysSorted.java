b15fd10c890f80b2c4dcc261f652feb4e6ff28d1
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-15 23:35:51 +0530

    Update CheckArraysSorted.java

--- Fundamentals/Recursion/Easy/CheckArraysSorted.java
+++ Fundamentals/Recursion/Easy/CheckArraysSorted.java
@@ -22,6 +22,24 @@ class Main {
       return false;
     }
   }
+  
+  
+  // Better way
+   public static boolean checkSorted(int [] arr , int index){
+        if(index == arr.length-1){
+        return true;
+      }
+
+      if(arr[index] > arr[index+1]){
+        return false;
+      }
+
+      boolean isSorted = checkSorted(arr,index+1);
+      return isSorted;
+      
+  }
+
+
   public static void main(String[] args) {
     int [] arr = {1};
     boolean  ans = checkSorted(arr);
