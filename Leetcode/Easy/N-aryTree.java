c40d64ac35030868f1e4961a42e642b4f1a59f14
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-19 23:40:07 +0530

    Update N-aryTree.java

--- Leetcode/Easy/N-aryTree.java
+++ Leetcode/Easy/N-aryTree.java
@@ -13,3 +13,26 @@
             preOrder(root.children.get(i) , list);
         }
     }
+// Using Stacks Iterative Approach
+class Solution {
+    public List<Integer> preorder(Node root) {
+       Stack<Node> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        stack.push(root);
+        
+        if(root == null ){
+            return list;
+        }
+        
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            list.add(node.val);
+            for(int i = node.children.size()-1;i>=0;i--){
+                stack.push(node.children.get(i));
+            }
+        }
+        
+        return list;
+    }
+    
+}
