cdb760ea6fa4b32fe6fa918851d3cac054a3b683
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-21 19:16:01 +0530

    559

--- Leetcode/Easy/MaximumDepthN-ary.java
+++ Leetcode/Easy/MaximumDepthN-ary.java
@@ -20,3 +20,33 @@ class Solution {
         
     }
 }
+
+
+// BFS using Queue
+class Solution {
+   
+    
+    public int maxDepth(Node root) {
+        
+        if(root == null) return 0;
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            
+            for(int i = 0; i<size;i++){
+                Node node = queue.poll();
+                for(Node child : node.children ){
+                    queue.add(child);
+                }
+                
+             }
+            
+            depth++;
+        }
+        
+            return depth;
+       }
+}
