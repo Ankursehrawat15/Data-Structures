a072fb9881bd2263b547e4878c93ca09cb739fd9
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-23 00:25:42 +0530

    lc:872

--- Leetcode/Easy/LeafSimilarTrees.java
+++ Leetcode/Easy/LeafSimilarTrees.java
@@ -1,8 +1,8 @@
 class Solution {
     
-    public ArrayList<Integer> leaf(TreeNode root1 , ArrayList<Integer> list){
+    public void leaf(TreeNode root1 , ArrayList<Integer> list){
        
-        if(root1 == null) return list;
+        if(root1 == null) return ;
         
         if(root1.left == null && root1.right == null){
             list.add(root1.val);
@@ -12,7 +12,7 @@ class Solution {
         leaf(root1.right , list);
         
         
-        return list;
+       
     }
     
     public boolean leafSimilar(TreeNode root1, TreeNode root2){
@@ -20,8 +20,8 @@ class Solution {
           ArrayList<Integer> list1 = new ArrayList<>();
           ArrayList<Integer> list2 = new ArrayList<>();
         
-          list1 = leaf(root1 , list1);
-          list2 = leaf(root2 , list2);
+          leaf(root1 , list1);
+          leaf(root2 , list2);
         
         if(list1.size() != list2.size()){
             return false;
@@ -37,5 +37,6 @@ class Solution {
         
         
         return true;
-      }
-}
+       
+        
+    }
