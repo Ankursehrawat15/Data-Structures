f6670ca05b45876eeb9492eade786be53776c7c0
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-24 17:39:17 +0530

    EASY

---
+++ Leetcode/Easy/postOrder.java
@@ -0,0 +1,21 @@
+// Recursive O(N)
+class Solution {
+    
+    public void postOrder(TreeNode root , List<Integer> list){
+        if(root == null){
+            return;
+        }
+        
+        postOrder(root.left , list);
+        postOrder(root.right , list);
+           list.add(root.val);
+        
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        
+       postOrder(root , list);
+        return list;
+    }
+}
