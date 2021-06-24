232526ae60200d645aabe86517262d1fbdf9f3d3
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-24 17:37:29 +0530

    EASY

---
+++ Leetcode/Easy/preOrder.java
@@ -0,0 +1,15 @@
+// Recursive O(N)
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null) return list;
+        
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        
+        return list;
+    }
+}
+
+// Iteratively 
