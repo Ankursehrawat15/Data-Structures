da5702b60d36fb1620ec49b36bfab4dcf906f61f
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-06-30 04:36:17 +0530

    Easy

---
+++ Leetcode/Easy/GreatestCandies.java
@@ -0,0 +1,23 @@
+  public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> list = new ArrayList<>(); 
+        int maxCandies = Integer.MIN_VALUE;
+        
+        for(int candy : candies){
+         if(candy > maxCandies){
+              maxCandies = candy;
+            }
+        }
+        
+        for(int candy : candies){
+            if(candy + extraCandies >= maxCandies){
+                list.add(true);
+            }else{
+                list.add(false);
+            }
+        }
+      
+        return list;
+        
+        
+    }
