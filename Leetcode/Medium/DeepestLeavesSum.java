4a76027bcb695a0cf20b1dae7e99ccdba375d4fc
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-07-01 07:34:14 +0530

    Medium

--- Leetcode/Medium/DeepestLeavesSum.java
+++ Leetcode/Medium/DeepestLeavesSum.java
@@ -22,3 +22,35 @@ class Solution {
        
     }
 }
+
+// using Recursion
+class Solution {
+    int sum  = 0;
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        
+        return 1 + Math.max(height(root.left) , height(root.right));
+    }
+    
+    public void sum (TreeNode root , int maxDepth){
+        if(root == null || maxDepth < 0 ) return;
+        
+        if(maxDepth == 0){
+            sum += root.val;
+        }
+        sum(root.left , maxDepth - 1);
+        sum(root.right,maxDepth-1);
+        
+        
+    }
+    
+    
+    public int deepestLeavesSum(TreeNode root) {
+     
+        int maxDepth = height(root) - 1;
+        sum(root,maxDepth);
+        
+        return sum;
+       
+    }
+}
