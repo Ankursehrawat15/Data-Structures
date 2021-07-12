4a5c0ffca6e70fedd924cee85a79c51c6853a98d
Ankursehrawat15 <ankursehrawat150999@gmail.com>
2021-07-12 18:00:18 +0530

    Easy

---
+++ Leetcode/Easy/RemoveElement.java
@@ -0,0 +1,43 @@
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int count = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                nums[i] = -1;
+                count--;
+            }
+        }
+
+        if (count == 0)
+            return count;
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+
+            if (nums[start] != -1) {
+                while (nums[start] != -1 && start < end) {
+                    start++;
+                }
+            }
+
+            if (nums[end] == -1) {
+                while (nums[end] == -1 && end > start) {
+                    end--;
+                }
+            }
+
+            if (start < end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        return count;
+    }
+}
