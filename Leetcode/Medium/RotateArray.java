bf205db3ef8000229902351bcafde42b34dd119d
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-08-21 23:49:31 +0530

    Medium

--- Leetcode/Medium/RotateArray.java
+++ Leetcode/Medium/RotateArray.java
@@ -19,3 +19,30 @@ class Solution {
             
     }
 }
+// Time complexity is O(N) and space complexity is O(1)
+class Solution {
+        
+        private void reverse(int [] nums , int s , int e){
+                
+                while(s < e){
+                        
+                        int temp = nums[e];
+                        nums[e] = nums[s];
+                        nums[s] = temp;
+                        
+                        s++;
+                        e--;
+                }
+        }
+        
+        
+        
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+          
+            reverse(nums , 0 , nums.length-1);
+            reverse(nums , 0 , k-1);
+            reverse(nums , k , nums.length-1);
+          
+    }
+}
