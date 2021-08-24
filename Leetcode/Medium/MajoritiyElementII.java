492088097254d356ab411d730c4a2b52b40a7d31
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-08-24 18:04:20 +0530

    Update MajoritiyElementII.java

--- Leetcode/Medium/MajoritiyElementII.java
+++ Leetcode/Medium/MajoritiyElementII.java
@@ -25,3 +25,49 @@ public List<Integer> majorityElement(int[] nums) {
             return list;
           
     }
+// Time Complexity is O(2N) and Space Complexity is O(1)
+// Moore's Voting Algorithm
+  public List<Integer> majorityElement(int[] nums) {
+         List<Integer> list = new ArrayList<>();
+            int n = nums.length;
+            int num1 =Integer.MIN_VALUE,num2 = Integer.MIN_VALUE , count1 = 0 , count2 = 0;
+           
+            for(int num : nums){
+                    if(num1 == num){
+                            count1++;
+                    }else if(num2 == num){
+                            count2++;
+                    }else if(count1 == 0){
+                            num1 = num;
+                            count1 = 1;
+                    }else if(count2 == 0){
+                            num2 = num;
+                            count2 = 1;
+                    }else{
+                            count1--;
+                            count2--;
+                    }
+            }
+            
+            int cnt1 = 0 , cnt2 = 0;
+            for(int num : nums){
+                   if(num1 == num){
+                           cnt1++;
+                   } 
+                    
+                    if(num2 == num){
+                            cnt2++;
+                    }
+            }
+            
+            if(cnt1 > n/3){
+                    list.add(num1);
+            }
+            
+            if(cnt2 > n/3){
+                    list.add(num2);
+            }
+            
+            
+            return list;
+    }
