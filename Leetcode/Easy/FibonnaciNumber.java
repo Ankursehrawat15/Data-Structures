8bc2155f553d3fa1b9f2e24b69baf531fe2a3e24
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-09-03 00:23:48 +0530

    Easy

--- Leetcode/Easy/FibonnaciNumber.java
+++ Leetcode/Easy/FibonnaciNumber.java
@@ -1,3 +1,34 @@
+// using DP Memorization
+ private int dp_fib(int n , int[] str){
+            
+            if(n == 1 || n == 0){
+                    str[n] = n;
+                    return str[n];
+            }
+            
+            
+            if(str[n] != -1){
+                    return str[n];
+            }
+            
+            str[n] = dp_fib(n-1 , str) + dp_fib(n-2 , str);
+            return str[n];
+    }
+        
+        
+        
+    public int fib(int n) {
+        
+     int [] arr = new int[n+1];
+            Arrays.fill(arr , -1);
+         
+            return dp_fib(n , arr);
+         
+    }
+
+
+
+
 public class FibonnaciNumber {
     // time complexity is very bad as we are doing repetitve work
     // will optimize this after doing DP
