ceb753ea5a1678244b398ab4ee9fb692925caedd
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-10-01 10:57:26 +0530

    Create MajortiyElement.java

---
+++ 250QuestionsSheet/Arrays/MajortiyElement.java
@@ -0,0 +1,28 @@
+// Using HashMaps
+// Time complexity is O(N) and Space Complexity is O(N)
+class Solution {
+    public int majorityElement(int[] nums) {
+            int n = nums.length;
+        HashMap<Integer , Integer> map = new HashMap<>();
+            
+            for(int i=0;i<nums.length;i++){
+                    
+                 map.put(nums[i] , map.getOrDefault(nums[i] , 0)+1);        
+            }
+            
+           
+            int key = -1;
+            for(Map.Entry mapElem : map.entrySet()){
+                    
+                    if((int)mapElem.getValue() > (n/2 ) ){
+                          
+                        key = (int) mapElem.getKey();
+                    }
+            }
+            
+            
+            return key;
+            
+            
+    }
+}
