2998aa6769c77887a12e3d81c3ed88464ed0d8f5
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-10-13 19:54:56 +0530

    Update fibonacci.java

--- 250QuestionsSheet/Arrays/fibonacci.java
+++ 250QuestionsSheet/Arrays/fibonacci.java
@@ -12,3 +12,42 @@ class Solution {
         
     }
 }
+
+// Using DP memorization
+class Solution {
+    public int fib(int n) {
+        
+      int [] dp = new int[n+1];
+		Arrays.fill(dp, -1);
+		return fib_dp(n , dp);
+        
+    }
+    
+    
+    
+    private static int fib_dp(int n , int [] dp) {
+		
+		if(n == 1 || n == 0) {
+			return n;
+		}
+		
+		int first_num;
+		if(dp[n-1] == -1) {
+			first_num = fib_dp(n-1 , dp);
+			dp[n-1] = first_num;
+		}else {
+			first_num = dp[n-1];
+		}
+		
+		int sec_num;
+		if(dp[n-2] == -1) {
+			sec_num = fib_dp(n-2 , dp);
+			dp[n-2] = sec_num;
+		}else {
+			sec_num = dp[n-2];
+		}
+		
+		return first_num + sec_num;
+		
+	}
+}
