808cb372c1097207a7606024f0d60d2fd253b6c9
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-10-25 15:32:17 +0530

    Update IntersectionLinkedList.java

--- 250QuestionsSheet/LinkedList/IntersectionLinkedList.java
+++ 250QuestionsSheet/LinkedList/IntersectionLinkedList.java
@@ -70,3 +70,53 @@ public class Solution {
          
     }
 }
+
+// Consize solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    
+ 
+    // 3.  start traversing and check for the same address of nodes
+    private ListNode intersection(ListNode headA , ListNode headB){
+         
+        ListNode listNode1 = headA;
+        ListNode listNode2 = headB;
+        while(headA != headB){
+            
+            if(headA == null){
+                headA= listNode2;
+            }else{
+                headA = headA.next;
+            }
+            
+            if(headB == null){
+                headB = listNode1;
+            }else{
+                headB= headB.next;
+            }
+            
+        }
+        
+        
+        return headA;
+        
+    }
+     // 2.  bigger length - smaller one = starting point.
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        if(headA == null || headB == null) return null;
+        
+        return intersection(headA , headB);
+         
+    }
+}
