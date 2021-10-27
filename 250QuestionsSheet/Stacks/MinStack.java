705f068c2f30bdb648750ba5228ab483c00bf93a
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2021-10-27 22:23:11 +0530

    Update MinStack.java

--- 250QuestionsSheet/Stacks/MinStack.java
+++ 250QuestionsSheet/Stacks/MinStack.java
@@ -40,6 +40,68 @@ class MinStack {
     }
 }
 
+// Using One stack only
+class MinStack {
+    
+    Stack<Long> stack;
+    long min;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+      long elem = Long.valueOf(val);
+        if(stack.isEmpty()){
+            min = elem;
+            stack.push(elem);
+            return;
+        }
+        
+        if(elem < min){
+            stack.push(elem + (elem - min));
+            min = elem;
+        }else{
+            stack.push(elem);
+        }
+        
+        
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()){
+            return;
+        }
+        
+        long elem = stack.pop();
+        if(elem < min){
+            min = 2 * min - elem;
+        }
+    }
+    
+    public int top() {
+     long elem = stack.peek();
+        if(elem > min){
+            return (int)elem;
+        }else{
+            return (int)min;
+        }
+    }
+    
+    public int getMin() {
+        return (int)min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
